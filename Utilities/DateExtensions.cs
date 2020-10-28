67638007497d526f077c049887cd07b99adfc9a9
bigchunguspng <70759405+bigchunguspng@users.noreply.github.com>
2020-10-28 22:15:17 +0200

    fix time display

--- Utilities/DateExtensions.cs
+++ Utilities/DateExtensions.cs
@@ -13,12 +13,14 @@ namespace TimeManager.Utilities
         public static string TimeSpanToString(TimeSpan time, string a = "")
         {
             if (time.Days > 1)
-                return $"{time.Days} days {a}";
+                return $"{time.Days} days {a}";                           //2 days - 386 days ...
+            if (time.Days > 0)
+                return $"day & {TimeSpanToString(time - TimeSpan.FromDays(1))} {a}";    //day & 0:00 - day & 23:59 hours
             if (time.Hours > 0)
-                return time.ToString(@"%h\:mm") + $" hours {a}";
+                return time.ToString(@"%h\:mm") + $" hours {a}";  //1:00 h - 23:59
             if (time.Minutes > 9)
-                return time.ToString(@"%m") + $" minutes {a}";
-            return time.ToString(@"%m\:ss") + $" {a}";
+                return time.ToString(@"%m") + $" minutes {a}";    //10 minutes - 59 minutes
+            return time.ToString(@"%m\:ss") + $" {a}";            //0:00 - 9:59
         }
 
         public static string DateAndTime(DateTime dateTime) => $"{DateOnly(dateTime)} {dateTime.TimeOfDay:%h\\:mm}";

4233844dd64759ad361c22e6abde0a70c552499a
bigchunguspng <70759405+bigchunguspng@users.noreply.github.com>
2021-03-05 22:15:22 +0200

    Better design, custom window chrome

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -7,6 +7,11 @@ namespace TimeManager.View
         public MainWindow()
         {
             InitializeComponent();
+
+            Minimize.Click += (sender, args) => WindowState = WindowState.Minimized;
+            Maximize.Click += (sender, args) =>
+                WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+            CloseWindow.Click += (sender, args) => Close();
         }
     }
 }

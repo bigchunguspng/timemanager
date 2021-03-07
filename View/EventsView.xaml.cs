6eeb597efa489c80ccd96ab6ce08e7eb795153fa
bigchunguspng <70759405+bigchunguspng@users.noreply.github.com>
2021-03-07 12:36:24 +0200

    Changed style of items and fixed scrolling in Events section

--- View/EventsView.xaml.cs
+++ View/EventsView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace TimeManager.View
 {
@@ -7,6 +8,13 @@ namespace TimeManager.View
         public EventsView()
         {
             InitializeComponent();
+        }
+
+        private void Scroll_OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ScrollViewer scrollViewer = (ScrollViewer) sender;
+            scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - e.Delta / 5f);
+            e.Handled = true;
         }
     }
 }
